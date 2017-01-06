acc05500a49615f5e0c8742976eaacf58b986746
Charles Clemens <cclemensjr@gmail.com>
2017-01-06 04:38:02 -0800

    Added feature returning the number of the specified string in the dictionary.

--- substrings/Substrings.rb
+++ substrings/Substrings.rb
@@ -2,3 +2,14 @@
 # an array of valid substrings (your dictionary) as the second argument. It
 # should return a hash listing each substring (case insensitive) that was found
 # in the original string and how many times it was found.
+
+def substrings word, dictionary
+  list_of_substrings = {word => dictionary.count(word)}
+  puts list_of_substrings
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low",
+              "own","part","partner","sit"]
+
+substrings("below", dictionary)
