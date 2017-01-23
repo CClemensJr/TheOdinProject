186af9e032973ab46c6a180d119c9c402f9f7f0e
Charles Clemens <cclemensjr@gmail.com>
2017-01-23 05:08:40 -0800

    Added ability to compare and switch elements in an array

--- bubble-sort/Bubble_Sort.rb
+++ bubble-sort/Bubble_Sort.rb
@@ -3,6 +3,15 @@
 #methodology (using #sort would be pretty pointless,
 #wouldn't it?).
 
-def bubble_sort
-  #insert code here!
+def bubble_sort arr
+  (0..arr.length - 1).each do |i|
+    puts "i = #{arr[i]} i + 1 = #{arr[i + 1]}"
+    if arr[i] > arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+      puts "Now sorted i = #{arr[i]} i + 1 = #{arr[i + 1]} "
+    end
+  end
 end
+
+bubble_sort([3, 2, 9, 6, 5])
