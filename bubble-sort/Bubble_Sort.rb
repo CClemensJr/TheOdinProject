f3253fe4332215ea54a0ed9736a7d34657acfa23
Charles Clemens <cclemensjr@gmail.com>
2017-01-25 04:17:24 -0800

    Refactored bubble sort method

--- bubble-sort/Bubble_Sort.rb
+++ bubble-sort/Bubble_Sort.rb
@@ -6,20 +6,13 @@
 def bubble_sort arr
   max_passes = arr.size - 1
   pass = 1
-  #arr.each do
-  while pass <= max_passes do
-    (0...arr.length).each do |i|
-      if arr[i + 1] == nil
-        break
-      elsif arr[i] > arr[i + 1]
-        puts "#{arr[i]} is bigger than #{arr[i + 1]} and sorting is needed\n"
 
+  while pass <= max_passes do
+    (0...max_passes).each do |i|
+      if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
-        puts "#{arr[i]} is now less than #{arr[i + 1]}. Sorting completed.\n"
         pass += 1
-      else
-        puts "#{arr[i]} is less than #{arr[i + 1]} and sorting is not needed\n"
       end
     end
   end
