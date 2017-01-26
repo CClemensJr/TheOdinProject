44675ac3f7188762b98f6530c543fd38887eae6e
Charles Clemens <cclemensjr@gmail.com>
2017-01-26 04:50:30 -0800

    Removed a whole bunch of unnecessary comments

--- bubble-sort-by/Bubble_Sort_By.rb
+++ bubble-sort-by/Bubble_Sort_By.rb
@@ -10,21 +10,7 @@ def bubble_sort_by arr
   max_passes = arr.size - 1
   pass = 1
 
-  #bubble_sort_array = (0...max_passes).each do |i|
-  #  if arr[i] > arr[i + 1]
-  #    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-  #    pass += 1
-  #  end
-  #end
-
   while pass <= max_passes do
-  #  if sort_style < 0
-  #    puts "Sort_Style = #{sort_style} Ascending"
-  #    bubble_sort_array
-  #  elsif sort_style > 0
-  #    puts "Sort_Style = #{sort_style} Descending"
-  #    bubble_sort_array.reverse!
-  #  end
     (0...max_passes).each do |i|
       if yield(arr[i], arr[i + 1]) > 0
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
@@ -42,7 +28,3 @@ end
 bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
   left.length - right.length
 end
-
-#bubble_sort_by([1, 7, 3]) do |left, right|
-#  left.length - right.length
-#end
