b6e577a2e107b0e176bb4305723cfbd2081b30f8
Charles Clemens <cclemensjr@gmail.com>
2017-01-26 13:00:59 -0800

    Started modifying enumerable methods

---
+++ enumerable-methods/Enumerable_Methods.rb
@@ -0,0 +1,15 @@
+# Create a script file to house your methods and run it in IRB to test them later. -- Check
+
+# Add your new methods onto the existing Enumerable module. Ruby makes this easy for you
+# because any class or module can be added to without trouble ... just do something like:
+    #module Enumerable
+    #  def my_each
+    #    # your code here
+    #  end
+    #end
+
+module Enumerable
+  def my_each
+    #my code here
+  end
+end
