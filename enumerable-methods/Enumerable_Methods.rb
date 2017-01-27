820afd055fb6129ddcf5e53349ab2c5dda49d912
Charles Clemens <cclemensjr@gmail.com>
2017-01-27 14:02:09 -0800

    Completed my_each method

--- enumerable-methods/Enumerable_Methods.rb
+++ enumerable-methods/Enumerable_Methods.rb
@@ -9,7 +9,13 @@
     #end
 
 module Enumerable
+  # Create #my_each, a method that is identical to #each but (obviously) does not
+  # use #each. You'll need to remember the yield statement. Make sure it returns
+  # the same thing as #each as well.
   def my_each
-    #my code here
+    0.upto(self.length - 1) { |x| yield(self[x]) }
   end
+
 end
+
+[0, 2, 4, 5].my_each { |i| puts i }
