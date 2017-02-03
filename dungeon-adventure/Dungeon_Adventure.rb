3b895a8559d908f55fe8947d592e3c5c85785429
Charles Clemens <cclemensjr@gmail.com>
2017-02-03 15:54:47 -0800

    Added the navigation feature

--- dungeon-adventure/Dungeon_Adventure.rb
+++ dungeon-adventure/Dungeon_Adventure.rb
@@ -25,10 +25,26 @@ class Dungeon
   def find_room_in_dungeon(reference)
     @rooms[reference]
   end
+
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player.location).connections[direction]
+  end
+  
+  def go(direction)
+    puts "You go " + direction.to_s
+    @player.location = find_room_in_direction(direction)
+    show_current_description
+  end
 end
 
 #lets player object keep track of player name and location
-Player = Struct.new(:name, :location)
+class Player
+  attr_accessor :name, :location
+
+  def initialize(name, location)
+    @name = name
+  end
+end
 
 #lets room objects store room name, description, connection to other rooms
 # and a reference, to be used by other rooms for their connections
