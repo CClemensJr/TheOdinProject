c736daed2061f14db0823f7fab14e48394ca762e
Charles Clemens <cclemensjr@gmail.com>
2017-02-03 11:22:29 -0800

    Added ability to tell which room the player is in

--- dungeon-adventure/Dungeon_Adventure.rb
+++ dungeon-adventure/Dungeon_Adventure.rb
@@ -12,6 +12,19 @@ class Dungeon
   def add_room(reference, name, description, connections)
     @rooms[reference] = Room.new(reference, name, description, connections)
   end
+
+  def start(location)
+    @player.location = location
+    show_current_description
+  end
+
+  def show_current_description
+    puts find_room_in_dungeon(@player.location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms[reference]
+  end
 end
 
 #lets player object keep track of player name and location
@@ -19,4 +32,17 @@ Player = Struct.new(:name, :location)
 
 #lets room objects store room name, description, connection to other rooms
 # and a reference, to be used by other rooms for their connections
-Room = Struct.new(:reference, :name, :description, :connection)
+class Room
+  attr_accessor :reference, :name, :description, :connections
+
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+
+  def full_description
+    @name + "\n\nYou are in " + @description
+  end
+end
